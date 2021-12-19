1281175802240e0a074df2808006db4c054cf821
Jack White <jfw793@gmail.com>
2021-12-19 13:14:18 +1100

    About to refactor the whole thing. God be with me.

--- src/components/Results/FetchResults.js
+++ src/components/Results/FetchResults.js
@@ -5,7 +5,7 @@ class FetchResults extends Component {
         const searchQuery = this.props.searchQuery
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=0099fa805ff60f1cae24e90d7be97930&query=${searchQuery}`)
           .then(response => response.json())
-          .then(data => this.props.handleState(false, data))
+          .then(data => this.props.handleState(false, data, ''))
     }
 
     componentDidMount() {
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,34 +1,50 @@
 import React, {Component} from 'react'
 import FetchResults from './FetchResults'
-import ResultsUI from './ResultsUI.js'
+import ResultsUI from './ResultsUI'
+import Movie from '../Movie/Movie'
 
 class Results extends Component {
   constructor() {
     super()
     this.state = {
       loading: true,
-      results: ''
+      results: '',
+      movieid: ''
     }
     this.handleState = this.handleState.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleState(isLoading, movieTitle) {
+  handleState(isLoading, movieTitle, id) {
     this.setState({
       loading: isLoading,
-      results: movieTitle
+      results: movieTitle,
+      movieid: id
     })
   }
 
   handleClick(id) {
     console.log(id)
+    this.setState({
+      loading: this.state.loading,
+      results: this.state.results,
+      movieid: id
+    }, () => console.log(this.state))
   }
 
   render() {
     return (
       <div>
-        <FetchResults handleState={this.handleState} searchQuery={this.props.searchQuery} results={this.state.results.results}/>
-        <ResultsUI searchQuery={this.props.searchQuery} results={this.state.results} handleClick={this.handleClick}/>
+        <FetchResults 
+          handleState={this.handleState} 
+          searchQuery={this.props.searchQuery}
+          results={this.state.results.results}
+        />
+        <ResultsUI
+          handleClick={this.handleClick}
+          searchQuery={this.props.searchQuery} 
+          results={this.state.results} 
+        />
       </div>
     )
   }
